02849af7eb61520aae8f32bf6c81f7666d87b31e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-17 16:53:02 +0800

    Delete parser directory

--- parser/README.md
+++
@@ -1,3 +0,0 @@
-[![Build Status](https://travis-ci.com/sdkgen/parser.svg?branch=master)](https://travis-ci.com/sdkgen/parser)
-
-# npm i @sdkgen/parser
--- parser/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./ast";
-export * from "./compatibility";
-export { AstJson, astToJson, jsonToAst, TypeDescription } from "./json";
-export * from "./lexer";
-export * from "./parser";
-export * from "./token";
