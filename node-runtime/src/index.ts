526f5c334925dc206c07c514ae5f2cd3a5476d41
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-17 16:52:43 +0800

    Delete node-runtime directory

--- node-runtime/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./api-config";
-export * from "./context";
-export * from "./encode-decode";
-export * from "./error";
-export * from "./http-client";
-export * from "./http-server";
-export * from "./test-wrapper";
